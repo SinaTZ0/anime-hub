77a5f0de4352a505e221ac8d4c27af78918bcc1d
SinaTZ <sinatofangsaz@gmail.com>
2023-11-09 14:59:03 +0330

    optimise image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-dark-blue bg-[url('/images/s1.webp')] bg-contain bg-bottom bg-no-repeat">
+    <div className="h-screen w-screen bg-dark-blue bg-[url('/images/s1rs2.webp')] bg-cover bg-bottom bg-no-repeat">
       <div id="Title" className="p-5">
         <p className="text-right text-6xl leading-normal text-slate-50">
           سلام به
@@ -14,7 +14,7 @@ export default function Home() {
           خوش آمدید
         </p>
       </div>
-      <div id="BButton" className="ml-[20vw] mt-[15vh]">
+      <div id="BButton" className="ml-[20vw] mt-[10vh]">
         <Button
           variant="outline"
           className="rounded-full border-2 bg-transparent px-10 py-7 text-2xl text-slate-100 focus:bg-transparent focus:text-slate-100 active:bg-amber-400"
